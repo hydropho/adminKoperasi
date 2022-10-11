ab330a02b1c6e47f75bf13bf94dd235d29e6449e
hydropho <aswinkhad@gmail.com>
2022-10-11 22:41:15 +0700

    update db

--- db_koperasi.sql
+++ db_koperasi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2022 at 10:49 AM
+-- Generation Time: Oct 11, 2022 at 05:40 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -74,7 +74,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`username`, `password`, `nama_lengkap`, `level`, `aktif`) VALUES
-('admin', '$2y$10$Dv.ntxdlWFkwI3rANQ.JX.c25wtCAtCRNp8VfQsAyHHyVfq.cHfFK', 'Aswin Khairu Adnan', 1, 0);
+('admin', '$2y$10$Dv.ntxdlWFkwI3rANQ.JX.c25wtCAtCRNp8VfQsAyHHyVfq.cHfFK', 'Aswin Khairu Adnan', 2, 1),
+('ghani', '$2y$10$K0Y85mZh7bEGzPANFeBqDOQmj7b1EYn5/oZxk/QiUa2VCTJKY7JTi', 'Fatha Ghani', 1, 2),
+('adam', '$2y$10$2jEU8NStfBNU55FktJXImO0t5tnzFeTK1UajmzGzKIxofVOcGoyK2', 'Adam Musyafa', 1, 1);
 
 -- --------------------------------------------------------
 
@@ -98,7 +100,138 @@ CREATE TABLE `userdata` (
 --
 
 INSERT INTO `userdata` (`username`, `nama_lengkap`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `no_hp`, `profil`) VALUES
-('admin', 'Aswin Khairu Adnan', 'Bogor', '2004-05-16', 'Laki - Laki', 'JL. BENGKALIS NO.411 RT.11/07CILUAR', '083147797580', 'default.jpg');
+('admin', 'Aswin Khairu Adnan', 'Bogor', '2004-05-16', 'Laki - Laki', 'JL. BENGKALIS NO.411 RT.11/07CILUAR', '083147797580', 'default.jpg'),
+('ghani', 'Fatha Ghani', 'Bogor', '2004-09-24', 'Laki - Laki', 'Bogor', '081212121212', 'default.jpg'),
+('adam', 'Adam Musyafa', 'Tangerang', '2004-06-28', 'Laki - Laki', 'Tangerang', '081223232323', 'default.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_access_menu`
+--
+
+CREATE TABLE `user_access_menu` (
+  `id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_access_menu`
+--
+
+INSERT INTO `user_access_menu` (`id`, `role_id`, `menu_id`) VALUES
+(1, 2, 1),
+(2, 2, 2),
+(3, 1, 1),
+(4, 2, 3),
+(5, 2, 4),
+(6, 2, 5),
+(7, 2, 6),
+(8, 2, 7),
+(9, 1, 3),
+(10, 1, 4),
+(11, 1, 5),
+(12, 1, 7);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_menu`
+--
+
+CREATE TABLE `user_menu` (
+  `id` int(11) NOT NULL,
+  `menu` varchar(128) NOT NULL,
+  `icon` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_menu`
+--
+
+INSERT INTO `user_menu` (`id`, `menu`, `icon`) VALUES
+(1, 'Dashboard', 'flaticon-025-dashboard'),
+(2, 'User', 'flaticon-381-user-9'),
+(3, 'Pinjaman', 'flaticon-381-add-3'),
+(4, 'Simpanan', 'flaticon-381-add-1'),
+(5, 'SHU', 'flaticon-045-heart'),
+(6, 'Laporan', 'flaticon-072-printer'),
+(7, 'Simulasi Pinjaman', 'flaticon-381-calculator-1');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_sub_menu`
+--
+
+CREATE TABLE `user_sub_menu` (
+  `id` int(11) NOT NULL,
+  `menu_id` int(11) NOT NULL,
+  `title` varchar(128) NOT NULL,
+  `url` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user_sub_menu`
+--
+
+INSERT INTO `user_sub_menu` (`id`, `menu_id`, `title`, `url`) VALUES
+(1, 1, 'Dashboard', 'admin'),
+(2, 2, 'Pengurus', 'user'),
+(3, 2, 'Anggota', ''),
+(4, 3, 'Tambah Pinjaman Baru', ''),
+(5, 3, 'Data Pinjaman', ''),
+(6, 3, 'Tagihan Pinjaman', ''),
+(7, 4, 'Data Simpanan', ''),
+(8, 4, 'Jenis Simpanan', ''),
+(9, 5, 'SHU', ''),
+(10, 6, 'Bukti Bayar', ''),
+(11, 6, 'Laporan', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `user_access_menu`
+--
+ALTER TABLE `user_access_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- AUTO_INCREMENT for table `user_menu`
+--
+ALTER TABLE `user_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `user_sub_menu`
+--
+ALTER TABLE `user_sub_menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
