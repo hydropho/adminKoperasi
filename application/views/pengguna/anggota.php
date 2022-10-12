d7020ae5dfe0074fc809d657b5a4555fd846eb3b
raupsss <androghanz@gmail.com>
2022-10-12 15:49:29 +0700

    membuat edit_form

--- application/views/pengguna/anggota.php
+++ application/views/pengguna/anggota.php
@@ -53,7 +53,7 @@
                                                 <td><?= $ud['no_hp']?></td>
                                                 <td>
 													<div class="d-flex">
-														<a href="#" class="btn btn-primary shadow btn-xs sharp me-1"><i class="fas fa-pencil-alt"></i></a>
+														<a href="<?= base_url('pengguna/edit_form'); ?>" class="btn btn-primary shadow btn-xs sharp me-1"><i class="fas fa-pencil-alt"></i></a>
 														<a href="#" class="btn btn-danger shadow btn-xs sharp"><i class="fa fa-trash"></i></a>
 													</div>												
 												</td>												
